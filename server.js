93167144506d45a610e7a075ab733e3d7999ca71
Kristone Brown <Keyystone4@gmail.com>
2023-05-15 09:53:49 -0500

    define crud routes

--- server.js
+++ server.js
@@ -2,8 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//CRUD ROUTES
+app.get('/dogs', (req, res) => {
+    res.send("hello world");
+})
+//get all dogs
+app.post('/dogs', (req, res) => {
+    res.send('hello dog');
+})
+//post create
+app.get('/dogs/:id', (req, res) => {
+    res.send('This is the GET')
+})
+// get single dog
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+//PUT update dog
+app.put('/dogs/:id', (req, res) => {
+    res.send('This is a PUT');
+})
+
+// DELETE dog
+app.delete('/dogs/:id', (req, res) => {
+    res.send('This is the DELETE')
 })
 
 app.listen(port, () => {
